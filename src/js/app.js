a9909122f187d46ae40f2b7053c324748aec0be9
andreytver84 <uvikmedium@mail.ru>
2022-11-12 20:19:41 +0300

    change methods

--- src/js/app.js
+++ src/js/app.js
@@ -9,17 +9,33 @@ class Team {
     constructor() {
         this.members = new Set();
     }
-    static add(char) {
+    add(char) {
         if (this.members.has(char)) {
             throw new Error('Персонаж уже в команде');
         } else {
             this.members.add(char);
+            //console.log('успешно');
         }                  
     }
-    static addAll(...char) {
+    addAll(...char) {
         this.members.add(...char);
+        //console.log('успешно');
     };
-    static toArray() {
+    toArray() {
         return Array.from(this.members);
     };
 }
+
+/* const team = new Team();
+
+const member1 = { name: 'Andrey' };
+const member2 = { name: 'Andrey' };
+const member3 = member1;
+
+team.addAll(member1,member2,member3,{ name: 'Andrey' }); // успешно
+
+
+team.add(member1); // успешно
+team.add(member2); // успешно
+team.add(member3); // ошибка
+team.add({ name: 'Andrey' }); // успешно */
